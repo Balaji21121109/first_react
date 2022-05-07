988c01b06122849093625d22ccfc0e3055375566
Balaji A <93375333+Balaji21121109@users.noreply.github.com>
2022-05-07 21:23:17 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import  ReactDOM  from "react-dom";
+
+var fname="balaji";
+var lname="A";
+
+
+ReactDOM.render(
+  <>
+  <h1>{`my first name is${fname} and my last name is${lname}`}</h1>
+  </>,document.getElementById("root")
+);
